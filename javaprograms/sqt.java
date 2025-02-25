ceaf8a2ca2512b182c1b6bd43bb1acbf7d5d5128
Thahirudeen <Thahirudeen317@gmail.com>
2025-02-25 23:01:08 +0530

    DSA quiz in prblm

---
+++ javaprograms/sqt.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class sqt {
+	public static void main(String[] args) {
+	Scanner sc =new Scanner(System.in);
+	int n=sc.nextInt();
+    System.out.println(sqrt(n));
+	}
+	public static int sqrt(int n){
+	int a=1;
+	for(int i=1;i*i<=n;i++){
+	a=i;
+	}return a;
+	}
+}
