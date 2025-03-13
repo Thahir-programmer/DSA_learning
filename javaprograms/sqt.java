a4ba5fad24f799728640aaf08a4c37655080929c
Thahirudeen <Thahirudeen317@gmail.com>
2025-03-13 22:32:37 +0530

    changed something

--- javaprograms/sqt.java
+++ javaprograms/sqt.java
@@ -3,6 +3,7 @@ public class sqt {
 	public static void main(String[] args) {
 	Scanner sc =new Scanner(System.in);
 	int n=sc.nextInt();
+	sc.close();
     System.out.println(sqrt(n));
 	}
 	public static int sqrt(int n){
